ede402a714e9aff86dfc4c18fc265ded30bf1aa1
dc <dc@dc>
2021-06-17 11:31:50 +0700

    init project

---
+++ BACKEND/src/main/java/com/auth/JwtSpringsecureApplication.java
@@ -0,0 +1,13 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringsecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringsecureApplication.class, args);
+    }
+
+}
